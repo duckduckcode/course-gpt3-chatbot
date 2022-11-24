2e71255d2fe1114a4182ff3b4ba13fed4646e126
Tanya Gray <153077+tanyagray@users.noreply.github.com>
2022-11-25 11:30:48 +1300

    ci: Configure Netlify deployment URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://wonderful-sunshine-5d0382.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
